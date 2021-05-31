bae95dbb91af2cd19fb9ee3687fb738444857259
tk-i <77842969+tk-i@users.noreply.github.com>
2021-05-31 17:09:08 +0900

    修正3

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,7 @@ class Item < ApplicationRecord
     validates :title
     validates :text
     validates :image
+    validates :price
   end
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid" }
   
