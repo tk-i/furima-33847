7a839f40fb4fc017992b00f49589cec3e4cbe8e7
tk-i <77842969+tk-i@users.noreply.github.com>
2021-06-06 10:59:58 +0900

    修正3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,7 +28,7 @@ class ItemsController < ApplicationController
 
   def update
     if @item.update(item_params)
-      redirect_to item_path
+      redirect_to item_path(@item)
     else
       render :edit
     end
